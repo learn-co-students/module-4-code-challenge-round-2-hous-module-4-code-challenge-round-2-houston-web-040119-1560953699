4ceef1ccc681bd1c6260905d64e64db5f3e5b89c
github username <github email address>
2019-06-19 10:25:44 -0500

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
 
+let API = "https://boiling-brook-94902.herokuapp.com/transactions"
 class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      allTransactions: [],
+      displayTransactions: [],
+      isLoading: true
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount(){
+    fetch(API)
+    .then(res=>res.json())
+    .then(data=>
+      this.setState({
+        allTransactions: data,
+        displayTransactions: data,
+        isLoading: false
+      }))
+  }
+
+ 
+  handleChange=(e)=> {
+    this.setState({
+      displayTransactions: this.state.allTransactions.filter(t=>t.description.toLowerCase().includes(e.target.parentElement.children[0].value.toLowerCase()))||this.state.allTransactions.filter(t=>t.category.toLowerCase().includes(e.target.parentElement.children[0].value.toLowerCase()))
+    })
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        {this.state.isLoading === true ? null : 
+        <div>
+        <Search handleChange={this.handleChange}/>
+        <TransactionsList allTransactions={this.state.displayTransactions}/>
+        </div>
+        }
       </div>
     )
   }
