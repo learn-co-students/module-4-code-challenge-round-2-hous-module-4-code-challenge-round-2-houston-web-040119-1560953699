1d9b9cbf263c06b438cc8134347892b3bcdb5201
no-relation <eddiec76@gmail.com>
2019-06-19 10:46:31 -0500

    d

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -5,8 +5,11 @@ import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state={
+      displayTransaction: transactions
+    }
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
@@ -14,16 +17,25 @@ class AccountContainer extends Component {
 
   }
 
-  handleChange(event) {
-    // your code here
+  handleChange=(event) =>{
+    const value = event.target.value
+    this.setState({
+
+      displayTransaction: this.state.displayTransaction.filter(trans => trans.description.includes(value))
+    
+    })
+  console.log(this.state.displayTransaction)
+  console.log(value)
+
   }
 
   render() {
-
+//  console.log(this.props.)
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search transe={this.handleChange}/>
+        {this.state.displayTransaction.map(trans => <TransactionsList trans={trans}/>)}
+        
       </div>
     )
   }
